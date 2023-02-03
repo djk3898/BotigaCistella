93246cde2f20173d2b1923d3ea5839a6769d354b
David <davidjuncatorr@gmail.com>
2023-02-03 11:26:20 +0100

    creació projecte

---
+++ Botiga/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //variables Botiga
+        string[] productesBotiga = {"Pera", "Yogur", "Pastanaga", "Patata", "Melón"};
+        double[] preus = {10, 12, 14, 15, 25, 132, 2, 3};
+        int nElementsBotiga = 3;
+
+        //variables Cistella
+        string[] productesCistella;
+        int[] quantitat;
+        int nElementsCistella;
+        double diners;
+
+        Mostrar(nElementsBotiga, productesBotiga, preus);
+    }
+    //afegir productes
+    public static void AfegirProducte(string producte, double preu)
+    {
+
+    }
+
+    //mostrar els productes de la botiga
+    public static void Mostrar(int nElem, string[] productes, double[] preus)
+    {
+        for (int i = 0; i < nElem; i++)
+        {
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine(productes[i] + "\t" + preus[i] + "€");
+        }
+        Console.WriteLine("-----------------------------------------");
+    }
+}
